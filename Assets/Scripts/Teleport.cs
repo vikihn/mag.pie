0a6fd5595c73264eb1c3a4847f9080796236de16
Nicole Lehmann <s2210872017@fhooe.at>
2023-05-28 00:37:16 +0200

    Teleport Update #1
    Teleport funktioniert wenn man ned so genau hinschaut

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+
+    public Transform Player;
+    public Transform Target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+private bool entered;
+
+private void Update()
+    {
+
+        if (entered && Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("Pressed");
+            Player.transform.position = Target.transform.position;
+        }
+
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        entered = true;
+        Debug.Log("Entered the Collider");
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        entered = false;
+        Debug.Log("Left the Collider");
+    }
+
+}

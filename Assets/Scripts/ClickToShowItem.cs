7cc63302bd48b49bcf3ad6ea74a7611dcd65b800
viki_hn <hengviktoria@gmail.com>
2023-06-05 13:28:23 +0200

    Asset Update #4

--- Assets/Scripts/ClickToShowItem.cs
+++ Assets/Scripts/ClickToShowItem.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class ClickToShowItem : MonoBehaviour
 {
     public GameObject targetItem; // Reference to the item that will appear
+    public GameObject objectA;
+    public GameObject objectB;
+
+    private bool isObjectAActive = true;
 
     private void OnMouseDown()
     {
@@ -14,6 +18,24 @@ public class ClickToShowItem : MonoBehaviour
             // Enable or instantiate the target item
             targetItem.SetActive(true); // Uncomment this line if the target item is already in the scene
             // Instantiate(targetItem, transform.position, Quaternion.identity); // Uncomment this line if the target item needs to be instantiated
+        }
+
+        SwitchObjects();
+    }
+
+    private void SwitchObjects()
+    {
+        if (isObjectAActive)
+        {
+            objectA.SetActive(false);
+            objectB.SetActive(true);
+            isObjectAActive = false;
+        }
+        else
+        {
+            objectA.SetActive(true);
+            objectB.SetActive(false);
+            isObjectAActive = true;
         }
     }
 }

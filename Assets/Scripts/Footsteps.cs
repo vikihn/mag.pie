41c602ece71c9779d9a21589af7c2a50358b66d2
viki_hn <hengviktoria@gmail.com>
2023-06-06 18:35:40 +0200

    Sound Update #5

---
+++ Assets/Scripts/Footsteps.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footsteps : MonoBehaviour
+{
+    public AudioSource footstepsSound, jumpSound;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D) ||
+            Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+        {
+            footstepsSound.enabled = true;
+        }
+
+        
+        else
+        {
+            footstepsSound.enabled = false;
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            jumpSound.enabled = true;
+            footstepsSound.enabled = false;
+        }
+
+        else
+        {
+            jumpSound.enabled = false;
+        }
+    }
+}
